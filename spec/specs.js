d078d52fbfce6436da612b79798919da5b448702
Tyler Finleyy <tylerfinley1@Tylers-MBP.home>
2015-09-18 14:45:04 -0700

    fixed spec to function

--- spec/specs.js
+++ spec/specs.js
@@ -1,15 +1,15 @@
 describe('pizzaOrder', function() {
   it('make a pizza with toppings', function() {
     var Pizza = new pizzaOrder("Kinda big", 1, "ham");
-    expect(testPizza.size).to.equal("Kinda big");
-    expect(testPizza.number).to.equal(1);
-    expect(testPizza.topping).to.equal("ham");
+    expect(Pizza.size).to.equal("Kinda big");
+    expect(Pizza.number).to.equal(1);
+    expect(Pizza.topping).to.equal("ham");
   });
 
   it('calculates total value of pizza based on size, toppings, and number', function() {
-    var testPizza = new pizzaOrder("Huge", 1, "pineapple");
+    var Pizza = new pizzaOrder("Huge", 1, "pineapple");
 
-    expect(testPizza.totalCost()).to.equal(8.25);
+    expect(Pizza.totalCost()).to.equal(8.25);
   });
 
 });
